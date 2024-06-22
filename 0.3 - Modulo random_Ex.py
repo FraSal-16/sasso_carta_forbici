c28be96f4d9de619f33da5ada6ae73aea5aaa33d
FraSal-16 <francescosalemme16@gmail.com>
2024-06-22 11:13:00 +0200

    Add files via upload

---
+++ 0.3 - Modulo random_Ex.py
@@ -0,0 +1,23 @@
+# Utilizzando il modulo random, realizza il gioco del sasso, carta, forbici:
+# - sasso vince su forbici
+# - carta vince su sasso
+# - forbici vince su carta
+# - ogni simbolo pareggia contro ogni simbolo uguale
+
+# Implementate l'algoritmo:
+# - definendo una funzione per giocare
+# - permettendo all'utente di scegliere tra sasso, carta e forbici
+# - registrando il numero di vittorie, pareggi e sconfitte
+# - continuando a far giocare l'utente fino a quando non decide di smettere
+
+# BONUS: registra i dati della partita su un file di testo, decidendo quale sia il formato migliore
+
+# BONUS 2: prevedi e testa una funzione per poter leggere da un file di testo i dati di una partita e visualizzarli
+
+# BONUS 3: i dati di ciascuna partita devono essere salvati in un file txt che riporta la data 
+# e l'ora della fine della partita (ad esempio se la partita finisce il 14/06/2024 alle 20:02, dovrà chiamarsi
+#                                   "2024_06_14_20_02.txt")
+
+# BONUS 4: prevedi una funzione per poter accedere a tutti i dati delle partite salvate e fare una statistica dei giocatori.
+#          Prevedi quindi di chiedere il nickname del giocatore prima di avviare la partita. Il nickname dovrà comparire 
+#          tra i dati del .txt 
